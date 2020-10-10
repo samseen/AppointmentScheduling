be39a9fcc19b1291661c2c2e872a0ad006b963e7
Samson Akanbi <samson.akanbi@outlook.com>
2020-10-11 00:38:44 +0100

    Added the AppointmentsNotificationPolicy

---
+++ AppointmentReminders/Domain/AppointmentsFinder.cs
@@ -0,0 +1,34 @@
+﻿using AppointmentReminders.Models;
+using AppointmentReminders.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppointmentReminders.Domain
+{
+    public class AppointmentsFinder
+    {
+        private readonly IAppointmentRepository _repository;
+        private readonly ITimeConverter _timeConverter;
+
+        public AppointmentsFinder(IAppointmentRepository repository, ITimeConverter timeConverter)
+        {
+            _repository = repository;
+            _timeConverter = timeConverter;
+        }
+
+        public IList<Appointment> FindAvailableAppointments(DateTime currentTime)
+        {
+            var appts = _repository.FindAll();
+            var availableAppointments = _repository.FindAll()
+                .Where(appointment =>
+                    new AppointmentsNotificationPolicy(
+                        appointment, _timeConverter)
+                    .NeedsToBeSent(currentTime));
+
+
+            return availableAppointments.ToList();
+        }
+    }
+}
