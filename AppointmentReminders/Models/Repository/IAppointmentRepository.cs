e0e186a4fb46f9a3540de2c4cfa083404bee3ebb
Samson Akanbi <samson.akanbi@outlook.com>
2020-10-11 00:25:18 +0100

    Added the IAppointmentRepository.

---
+++ AppointmentReminders/Models/Repository/IAppointmentRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppointmentReminders.Models.Repository
+{
+    public interface IAppointmentRepository
+    {
+        void Create(Appointment appointment);
+        void Update(Appointment appointment);
+        void Delete(int id);
+        Appointment FindById(int id);
+        IEnumerable<Appointment> FindAll();
+    }
+}
