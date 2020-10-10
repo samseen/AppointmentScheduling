c5f6d9714f34bf913e256c0ad5ee8372dd9fdc07
Samson Akanbi <samson.akanbi@outlook.com>
2020-10-10 23:47:46 +0100

    Installed Hangfire package.

---
+++ AppointmentReminders/Hangfire.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Hangfire;
+using System.Web.Http;
+
+namespace AppointmentReminders
+{
+    public class Hangfire
+    {
+        public static void ConfigureHangFire(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage("DefaultConnection");
+
+            app.UseHangfireDashboard("/jobs");
+            app.UseHangfireServer();
+        }
+
+        public static void InitiateJobs()
+        {
+            RecurringJob.AddOrUpdate<Workers.SendNotificationsJob>(job => job.Execute(), Cron.Minutely);
+        }
+    }
+}
