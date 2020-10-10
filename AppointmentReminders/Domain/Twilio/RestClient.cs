b1a4878cf7e4e99dab6125b3fb95f0ec0f5d3864
Samson Akanbi <samson.akanbi@outlook.com>
2020-10-10 23:58:27 +0100

    Created a RestClient class.

---
+++ AppointmentReminders/Domain/Twilio/RestClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppointmentReminders.Domain.Twilio
+{
+    public class RestClient
+    {
+        private readonly ITwilioRestClient _client;
+        private readonly string _accountSid = WebConfigurationManager.AppSettings["AccountSid"];
+        private readonly string _authToken = WebConfigurationManager.AppSettings["AuthToken"];
+        private readonly string _twilioNumber = WebConfigurationManager.AppSettings["TwilioNumber"];
+
+        public RestClient()
+        {
+            _client = new TwilioRestClient(_accountSid, _authToken);
+        }
+
+        public RestClient(ITwilioRestClient client)
+        {
+            _client = client;
+        }
+
+        public void SendSmsMessage(string phoneNumber, string message)
+        {
+            var to = new PhoneNumber(phoneNumber);
+            MessageResource.Create(
+                to,
+                from: new PhoneNumber(_twilioNumber),
+                body: message,
+                client: _client);
+        }
+    }
+}
