5f9faecb32ad42c24de855e70ff096f96a6dfb18
Samson Akanbi <samson.akanbi@outlook.com>
2020-10-10 23:52:26 +0100

    Added a Startup class.

---
+++ AppointmentReminders/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AppointmentReminders.Startup))]
+namespace AppointmentReminders
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Hangfire.ConfigureHangFire(app);
+            Hangfire.InitiateJobs();
+        }
+    }
+}
