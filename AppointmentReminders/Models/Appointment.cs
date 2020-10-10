7d980f74c90e1f3936d5a0bcc73dc5db37260d78
Samson Akanbi <samson.akanbi@outlook.com>
2020-10-10 23:19:56 +0100

    Added the Appointment view.

--- AppointmentReminders/Models/Appointment.cs
+++ AppointmentReminders/Models/Appointment.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace AppointmentReminders.Web.Models
+namespace AppointmentReminders.Models
 {
     public class Appointment
     {
